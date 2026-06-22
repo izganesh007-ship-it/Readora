26848cdcdb2fe9bf8a33dc808d0ef52526595d67
Izganesh <izganesh007@gmail.com>
2026-06-22 22:45:01 +0545

    Use Railway PORT env var

--- app.py
+++ app.py
@@ -525,7 +525,8 @@ def init_db():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    port=int(os.getenv('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
 
 
 # AUTO-INIT FOR RENDER
